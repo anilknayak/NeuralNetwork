b84211fadbfd7ac30ffa3ff1ff1489b1ffe0f0ea
Anil Nayak <anilnayak@Anils-MacBook-Pro.local>
2017-10-24 17:39:02 -0500

    moified

--- run_training.sh
+++ run_training.sh
@@ -13,10 +13,10 @@ rm /home/anil/training_details/training/data/train* /home/anil/training_details/
 python SplitTrainingTestingData.py /home/anil/training_details/training_images/VOCdevkit/VOC2012/Annotations /home/anil/training_details/training_images/VOCdevkit/VOC2012/JPEGImages /home/anil/training_details/training/images
 python SplitTrainingTestingData.py /home/anil/training_details/training_images/dataset /home/anil/training_details/training_images/dataset /home/anil/training_details/training/images
 
-python xml_to_csv.py /home/anil/training_details/training/images /home/anil/training_details/training
+python xml_to_csv.py /home/anil/training_details/training/ /home/anil/training_details/training/
 
-python generate_tfrecord.py --csv_input=/home/anil/training_details/training/data/train_labels.csv --output_path=/home/anil/training_details/training/data/train.record --image_path=/home/anil/training_details/training
-python generate_tfrecord.py --csv_input=/home/anil/training_details/training/data/test_labels.csv --output_path=/home/anil/training_details/training/data/test.record --image_path=/home/anil/training_details/training
+python generate_tfrecord.py --csv_input=/home/anil/training_details/training/data/train_labels.csv --output_path=/home/anil/training_details/training/data/train.record --image_path=/home/anil/training_details/training/
+python generate_tfrecord.py --csv_input=/home/anil/training_details/training/data/test_labels.csv --output_path=/home/anil/training_details/training/data/test.record --image_path=/home/anil/training_details/training/
 
 
 cp -a /home/anil/training_details/training/data/* /home/anil/models/research/object_detection/data/
