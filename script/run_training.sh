4d90a2ff6e54c43642694f3411085b0a132d0d14
Anil Nayak <anilnayak@Anils-MacBook-Pro.local>
2017-10-25 12:40:08 -0500

    models

--- script/run_training.sh
+++ script/run_training.sh
@@ -8,7 +8,14 @@ mkdir /home/anil/training_details/training/images/test
 rm /home/anil/models/research/object_detection/data/*
 rm -R /home/anil/models/research/object_detection/images/
 rm /home/anil/models/research/object_detection/training/*
+rm /home/anil/training_details/training/data/*
+rm /home/anil/training_details/training/training/*
 rm /home/anil/training_details/training/data/train* /home/anil/training_details/training/data/test*
+
+
+cp -a /home/anil/training_details/model/object-detection.pbtxt /home/anil/training_details/training/data/
+cp -a /home/anil/training_details/model/ssd_mobilenet_v1_coco.config /home/anil/training_details/training/data/
+cp -a /home/anil/training_details/model/ssd_mobilenet_v1_coco.config /home/anil/training_details/training/training/
 
 python SplitTrainingTestingData.py /home/anil/training_details/training_images/VOCdevkit/VOC2012/Annotations /home/anil/training_details/training_images/VOCdevkit/VOC2012/JPEGImages /home/anil/training_details/training/images
 python SplitTrainingTestingData.py /home/anil/training_details/training_images/dataset /home/anil/training_details/training_images/dataset /home/anil/training_details/training/images
@@ -22,6 +29,9 @@ python generate_tfrecord.py --csv_input=/home/anil/training_details/training/dat
 cp -a /home/anil/training_details/training/data/* /home/anil/models/research/object_detection/data/
 cp -a /home/anil/training_details/training/images/ /home/anil/models/research/object_detection/
 cp -a /home/anil/training_details/training/training/* /home/anil/models/research/object_detection/training/
+
+
+
 
 
 
